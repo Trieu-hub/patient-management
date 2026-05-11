92c0366b4f03996466aa5a6450635913633cc5d9
nguyenquoctrieu9a52006@gmail.com <nguyenquoctrieu9a52006@gmail.com>
2026-05-11 22:20:31 +0700

    đã fix được auth-service container và thêm login request và response

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -17,7 +17,7 @@ public class JwtUtil {
 
   private final Key secretKey;
 
-  public JwtUtil(@Value("${jwt.secret}") String secret) {
+  public JwtUtil(@Value("${JWT_SECRET}") String secret) {
     byte[] keyBytes = Base64.getDecoder()
         .decode(secret.getBytes(StandardCharsets.UTF_8));
     this.secretKey = Keys.hmacShaKeyFor(keyBytes);
